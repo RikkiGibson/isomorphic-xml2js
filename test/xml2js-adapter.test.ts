09ee74a13506a4c4148d00772c5f8a7bed201d48
Rikki Gibson <rikkigibson@gmail.com>
2018-05-20 13:52:16 -0700

    Move node-xml2js tests to in-repo

--- test/xml2js-adapter.test.ts
+++ test/xml2js-adapter.test.ts
@@ -2,10 +2,11 @@
 function adaptTests(suiteName: string, module: any) {
   describe(suiteName, () => {
     for (const key of Object.keys(module)) {
-      it(key, (done) => {
+      it(key, function(done) {
         try {
           module[key]({
             done,
+            skip: () => this.skip(),
             finish: done,
             fail: (err: any) => {
               if (!(err instanceof Error)) {
@@ -25,7 +26,7 @@ function adaptTests(suiteName: string, module: any) {
   });
 }
 
-adaptTests("node-xml2js bom", require("../node-xml2js/test/bom.test.coffee"));
-adaptTests("node-xml2js builder", require("../node-xml2js/test/builder.test.coffee"));
-adaptTests("node-xml2js parser", require("../node-xml2js/test/parser.test.coffee"));
-adaptTests("node-xml2js processors", require("../node-xml2js/test/processors.test.coffee"));
+adaptTests("node-xml2js bom", require("./node-xml2js/bom.test.coffee"));
+adaptTests("node-xml2js builder", require("./node-xml2js/builder.test.coffee"));
+adaptTests("node-xml2js parser", require("./node-xml2js/parser.test.coffee"));
+adaptTests("node-xml2js processors", require("./node-xml2js/processors.test.coffee"));
