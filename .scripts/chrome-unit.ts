9dc5e53c485fcac95472921b3339495a8714e0ea
Rikki Gibson <rikkigibson@gmail.com>
2018-05-21 09:50:24 -0700

    Use more reliable test script for browser tests

---
+++ .scripts/chrome-unit.ts
@@ -0,0 +1,17 @@
+import { spawn, exec } from "child_process";
+import { join } from "path";
+
+const webpackDevServer = spawn(join(__dirname, "../node_modules/.bin/ts-node"), [join(__dirname, "../testserver")], { shell: true })
+
+let mochaChromeRunning = false
+const webpackDevServerHandler = async (data: Buffer) => {
+  if (!mochaChromeRunning) {
+    mochaChromeRunning = true
+    const mochaChrome = spawn(join(__dirname, "../node_modules/.bin/mocha-chrome"), ["http://localhost:3004"], { shell: true, stdio: "inherit" });
+    mochaChrome.on("exit", () => webpackDevServer.kill());
+  }
+}
+
+webpackDevServer.stderr.on('data', data => console.error(data.toString()));
+webpackDevServer.stdout.on('data', webpackDevServerHandler);
+webpackDevServer.on('exit', webpackDevServerHandler);
