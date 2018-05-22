3f205580aff5d2aae2390d19484b6b72feaf42e5
Rikki Gibson <rikkigibson@gmail.com>
2018-05-22 09:13:11 -0700

    Make processors visible in type declarations

--- lib/processors.ts
+++ lib/processors.ts
@@ -1,18 +1,18 @@
 
-exports.normalize = (str: string) => str.toLowerCase();
+export const normalize = (str: string) => str.toLowerCase();
 
-exports.firstCharLowerCase = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
+export const firstCharLowerCase = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
 
-exports.stripPrefix = (str: string) => str.replace(/(?!xmlns)^.*:/, '');
+export const stripPrefix = (str: string) => str.replace(/(?!xmlns)^.*:/, '');
 
-exports.parseNumbers = (str: any) => {
+export const parseNumbers = (str: any) => {
   if (!isNaN(str)) {
     str = str % 1 === 0 ? parseInt(str, 10) : parseFloat(str);
   }
   return str;
 };
 
-exports.parseBooleans = (str: any) => {
+export const parseBooleans = (str: any) => {
   if (/^(?:true|false)$/i.test(str)) {
     str = str.toLowerCase() === 'true';
   }
